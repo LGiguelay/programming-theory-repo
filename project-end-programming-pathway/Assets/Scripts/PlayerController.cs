a564b0b9838fccb234127cc17a382d498e91e78d
LGiguelay <47250304+LGiguelay@users.noreply.github.com>
2022-09-21 23:30:25 +0200

    Added Moving Head
    -Added a control to move the camera and rotate the player

--- project-end-programming-pathway/Assets/Scripts/PlayerController.cs
+++ project-end-programming-pathway/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float sensitivity;
+    [SerializeField] private GameObject head;
+
+    private float yRotation;
+    private float xRotation;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        if(Mathf.Abs(sensitivity) < 0.001)
+        {
+            Debug.Log("Sensitivity is zero");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveHead();   
+    }
+
+    private void MoveHead()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity; //horizontal head moving
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity; //vertical head moving
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(0, yRotation, 0);
+        head.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
     }
 }
