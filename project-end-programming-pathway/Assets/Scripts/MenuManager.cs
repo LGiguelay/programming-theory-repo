074fa36d24e691cfaa92bc42b48c080adebf3854
LGiguelay <47250304+LGiguelay@users.noreply.github.com>
2022-09-21 22:47:10 +0200

    Settings Scene + Game scene
    -Added Button functionality to go from Menu to Setting and to Game
    -Added Button functionality to go from Settings to Menu
    -Added ground and empty player in Game

--- project-end-programming-pathway/Assets/Scripts/MenuManager.cs
+++ project-end-programming-pathway/Assets/Scripts/MenuManager.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     public void StartGame()
     {
-
+        SceneManager.LoadScene("Game");
     }
 
     public void OpenSettings()
     {
-
+        SceneManager.LoadScene("Settings");
     }
 
     public void Exit()
